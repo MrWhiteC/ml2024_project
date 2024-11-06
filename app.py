4a8c9eb0fac93d961c6bdaee3e279bd715739f13
tada suttaket <tada.sutt@gmail.com>
2024-11-06 09:40:05 +0700

    no message

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/co2_emission')
+def co2_emission():
+    return render_template('co2_emission.html')
+
+@app.route('/planting')
+def planting():
+    return render_template('planting.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
